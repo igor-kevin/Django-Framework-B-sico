67af0c9d2d3b6a4c3e11cb6da1df58168e531d3c
Igor Kevin <igor_kevin@hotmail.com>
2024-08-22 11:26:13 -0300

    Criação do modelo de Cliente

--- core/models.py
+++ core/models.py
@@ -5,3 +5,9 @@ class Produto(models.Model):
     nome: str = models.CharField('Nome', max_length=100)
     preco: float = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     estoque: int = models.IntegerField('Quantidade em Estoque')
+
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('Sobrenome', max_length=100)
+    email = models.EmailField('E-mail', max_length=100)
