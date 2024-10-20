fd8b63bf890799101e866aa26c8759949c20aa54
Igor Kevin <igor_kevin@hotmail.com>
2024-10-20 20:19:02 -0300

    Commit django banco de dados no template Básico

--- core/views.py
+++ core/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
+from core.models import Produto
 
 
 def index(request) -> render:
+    produtos = Produto.objects.all()
     context = {
         'curso': 'Programação web com Django Framework',
-        'info': 'Mais info para o programa.'
+        'info': 'Mais info para o programa.',
+        'produtos': produtos
     }
     return render(request, 'index.html', context)
 
 
 def contato(request) -> render:
     return render(request, 'contato.html')
+
+
+def produto(request, pk) -> render:
+    prod = Produto.objects.get(id=pk)
+    context = {
+        'produto': prod
+    }
+    return render(request, 'produto.html', context=context)
--- django1/settings.py
+++ django1/settings.py
@@ -116,7 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/'  # Usado durante o desenvolvimento !
+STATIC_ROOT = Path(BASE_DIR, 'staticfiles')  # Usado durante a produção !
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
